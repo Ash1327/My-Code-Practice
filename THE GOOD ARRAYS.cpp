845ea4f1640a97fca45c947d6ffa21e0a7fecfa2
Ashish Sharma <143902454+Ash1327@users.noreply.github.com>
2023-09-08 21:20:03 +0530

    Create THE GOOD ARRAYS.cpp

---
+++ THE GOOD ARRAYS.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long t;
+    cin>>t;
+    while(t--){
+        float n,k;
+        cin>>n>>k;
+ 
+        float d = ceil((n-1)/k);
+        cout<<d+1<<endl;
+ 
+        
+    }
+}
