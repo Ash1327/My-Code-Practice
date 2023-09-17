92c48455d1402902c70b49d24becb8ac28a3916c
Ashish Sharma <143902454+Ash1327@users.noreply.github.com>
2023-09-17 23:19:41 +0530

    Create Sum of round Numbers.cpp

---
+++ Sum of round Numbers.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long t;
+    cin>>t;
+    while(t--){
+        long long n;
+        cin>>n;
+ 
+        vector<long long >ans;
+        while(n>0){
+            int power = 1;
+		while (n > 0) {
+			if (n % 10 > 0) {
+				ans.push_back((n % 10) * power);
+			}
+			n /= 10;
+			power *= 10;
+		}
+		cout << ans.size() << endl;
+		for (auto number : ans) cout << number << " ";
+		cout << endl;
+ 
+        }
+    }
+}
