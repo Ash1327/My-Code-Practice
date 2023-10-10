d590938d50f3f8f6ae827316456b54011e0e59d9
Ashish Sharma <143902454+Ash1327@users.noreply.github.com>
2023-10-10 22:56:26 +0530

    Create ABC.cpp

---
+++ ABC.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long long t;
+    cin>>t;
+    while(t--){
+        long long n;
+        cin>>n;
+        string s;
+        cin>>s;
+
+       if(n>2 ||s=="11" || s=="00"){
+        cout<<"NO"<<endl;
+       }
+       else{
+        cout<<"YES"<<endl;
+       }
+    }
+}
